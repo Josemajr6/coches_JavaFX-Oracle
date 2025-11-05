029087b3e6d2f8560c1ec9acf05bc02a8b4c96d3
José Manuel <josemajimenezrodriguez8@gmail.com>
2025-11-05 17:48:18 +0100

    Creación del proyecto con Maven

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module javafx2526.coches_JavaOracle {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens javafx2526.coches_JavaOracle to javafx.fxml;
+    exports javafx2526.coches_JavaOracle;
+}
