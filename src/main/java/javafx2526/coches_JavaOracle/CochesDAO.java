16baefee07ba8e7a7950b84dbf99368ffdc649f1
José Manuel <josemajimenezrodriguez8@gmail.com>
2025-11-06 21:44:05 +0100

    Eliminado algunos archivos por defecto de Maven y creación de CochesDAO con el método seleccionar

---
+++ src/main/java/javafx2526/coches_JavaOracle/CochesDAO.java
@@ -0,0 +1,40 @@
+package javafx2526.coches_JavaOracle;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class CochesDAO {
+
+	public ObservableList<Coche> seleccionar() {
+		ObservableList<Coche> coches = FXCollections.observableArrayList();
+		Connection con = Conexion.conectar();
+		
+		String sql = "select matricula, marca, modelo, km from PERSONAS";
+		
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				coches.add(new Coche(rs.getString(1), rs.getString(2), rs.getString(3), Integer.parseInt(rs.getString(4))));
+			}
+			
+			con.close();
+			con = null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return coches;
+		
+	}
+	
+	
+	
+	
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module javafx2526.coches_JavaOracle {
     requires javafx.controls;
     requires javafx.fxml;
 	requires java.sql;
+	requires javafx.base;
 
     opens javafx2526.coches_JavaOracle to javafx.fxml;
     exports javafx2526.coches_JavaOracle;
